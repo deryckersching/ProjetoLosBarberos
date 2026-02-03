9e593f3c014f003fe2758b7962337be567911f15
deryckersching <deryckersching@gmail.com>
2026-02-03 19:59:54 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# "Capa do Projeto"
+
---
+++ index.js
@@ -0,0 +1,57 @@
+// arquivo de entrada principal da aplicação
+
+require('dotenv').config();
+
+const express = require('./app')
+
+const PORT = process.env.PORT || 3001
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
